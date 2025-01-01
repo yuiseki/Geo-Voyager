5adc9c1033c9556c8b9f0c339981d8c6908413c7
yuiseki <yuiseki@gmail.com>
2025-01-02 08:58:28 +0900

    name除去、npx prisma generate

--- src/db/analyzeSkill.ts
+++ src/db/analyzeSkill.ts
@@ -1,12 +1,8 @@
 import { prisma } from ".";
 
 // AnalyzeSkillを作成する関数
-export const createAnalyzeSkill = async (
-  name: string,
-  description: string,
-  code: string
-) => {
+export const createAnalyzeSkill = async (description: string, code: string) => {
   return await prisma.analyzeSkill.create({
-    data: { name, description, code },
+    data: { description, code },
   });
 };
--- src/db/fetchSkill.ts
+++ src/db/fetchSkill.ts
@@ -1,12 +1,8 @@
 import { prisma } from ".";
 
 // FetchSkillを作成する関数
-export const createFetchSkill = async (
-  name: string,
-  description: string,
-  code: string
-) => {
+export const createFetchSkill = async (description: string, code: string) => {
   return await prisma.fetchSkill.create({
-    data: { name, description, code },
+    data: { description, code },
   });
 };
