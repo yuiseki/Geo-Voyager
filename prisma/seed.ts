a0d38c9d70751cbe084a6232e0ed989d4873c5d8
yuiseki <yuiseki@gmail.com>
2025-01-07 05:50:44 +0900

    WIP

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,15 +6,24 @@ import { glob } from "glob";
 const prisma = new PrismaClient();
 
 /*
-東京都においては、人口密度が高い行政区ほど、1人当たりの公園面積が小さい。
-東京都においては、鉄道路線の数が多い行政区ほど、駅の密度も高い。
-東京都においては、行政区の面積が広いほど、学校の数が多い。
-東京都においては、行政区の面積が広いほど、病院の数が多い。
-東京都においては、行政区の面積が広いほど、図書館の数が多い。
-東京都においては、行政区の面積が広いほど、公園の数が多い。
-東京都においては、商業施設の多い行政区ほど、駐車場の数が多い。
-東京都においては、駅の密度が高い行政区ほど、自転車駐輪場が多い。
-東京都においては、駅の密度が高い行政区ほど、商業施設が多い。
+世界でGDPが最も高い国はどこだろう？
+世界で森林面積の比率が最も高い国はどこだろう？
+世界の主要都市のなかで、面積あたりのレストラン数が最も多い都市はどこだろう？
+世界の主要都市のなかで、自転車用駐輪場の数が最も多い都市はどこだろう？
+世界で乳児死亡率（Infant Mortality Rate）が最も低い国はどこだろう？
+世界で女性の労働参加率（Female Labor Force Participation Rate）が最も高い国はどこだろう？
+世界の主要都市のなかで、人口に対する公園の面積が最も高い都市はどこだろう？
+世界で農業がGDPに占める割合（農業部門比率）が最も高い国はどこだろう？
+世界で都市化率（Urban population % of total population）が最も低い国はどこだろう？
+世界の主要都市のなかで、公共図書館（libraryタグ等）の数が最も多い都市はどこだろう？
+東京都において、公園面積が最も広い行政区はどこだろう？
+東京都において、商業施設が最も多い行政区はどこだろう？
+東京都において、1人あたりの文化施設が最も多い行政区はどこだろう？
+東京都において、交通機関の数が最も多い行政区はどこだろう？
+世界で、鉄道網が最も発達している国はどこだろう？
+東京都において、高齢者向け施設が最も多い行政区はどこだろう？
+東京都において、大学の数が最も多い行政区はどこだろう？
+東京都において、人口あたりの図書館の数が最も多い行政区はどこだろう？
 */
 
 const seedQuestionWhichCountryIsMostHighestPopulationDensity = async () => {
@@ -363,6 +372,17 @@ const seedQuestionWhichWardsInTokyoIsMostHighestDensitySchools = async () => {
   });
 };
 
+const seedQuestionWhichWarsInTokyoIsMostHighestDensityParks = async () => {
+  // Questionの作成
+  const question = await prisma.question.create({
+    data: {
+      description:
+        "東京都において、人口あたりの公園の面積が最も高い行政区はどこだろう？",
+      status: "OPEN",
+    },
+  });
+};
+
 const seedSkills = async () => {
   // src/lib/skills/**/*.ts ファイルをすべて取得
   const skillFiles = await glob(
@@ -403,6 +423,7 @@ async function main() {
   await seedQuestionWhichWardsInTokyoIsMostSchools();
   await seedQuestionWhichWardsInTokyoIsMostHighestDensityHospitals();
   await seedQuestionWhichWardsInTokyoIsMostHighestDensitySchools();
+  await seedQuestionWhichWarsInTokyoIsMostHighestDensityParks();
   await seedSkills();
 }
 
