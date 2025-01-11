7e78b8d639d10720d20d6f56d47965c2cb881230
yuiseki <yuiseki@gmail.com>
2025-01-11 10:27:36 +0900

    WIP

--- src/utils/listUpAllSolvedQuestions.ts
+++ src/utils/listUpAllSolvedQuestions.ts
@@ -1,9 +1,23 @@
 import { getAllQuestionsByStatus, QuestionStatus } from "../db/question";
+import { getAllExecutedTasksByQuestionId, TaskStatus } from "../db/task";
 
 export const listUpAllSolvedQuestions = async () => {
   const questions = await getAllQuestionsByStatus(QuestionStatus.SOLVED);
   console.log(`📚 Total ${questions.length} solved questions:`);
   for (const question of questions) {
     console.log(`- ✨️ ${question.description}`);
+    const executedTasks = await getAllExecutedTasksByQuestionId(question.id);
+    if (executedTasks.length > 0) {
+      console.log("📚 Executed tasks:");
+      for (const task of executedTasks) {
+        if (task.status === TaskStatus.COMPLETED) {
+          console.log(
+            `  - ✅ Task: ${task.description} - ${task.result} [${task.status}]`
+          );
+        } else {
+          console.log(`  - ❌ Task: ${task.description} [${task.status}]`);
+        }
+      }
+    }
   }
 };
