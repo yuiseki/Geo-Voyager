c4fa19a16e71b330fe4629963f2eabc7390b9361
yuiseki <yuiseki@gmail.com>
2025-01-11 10:33:49 +0900

    WIP

--- src/lib/skills/admins/Japan/Tokyo/findWardWithMostSchools.ts
+++ src/lib/skills/admins/Japan/Tokyo/findWardWithMostSchools.ts
@@ -65,15 +65,13 @@ out tags;
  * @param wardName - The name of the ward to query.
  * @returns The total count of schools in the ward.
  */
-async function getSchoolCountByWard(
-  wardName: string
-): Promise<number> {
+async function getSchoolCountByWard(wardName: string): Promise<number> {
   const overpassQuery = `
 [out:json];
 area["name"="東京都"]->.out;
 area["name"="${wardName}"]->.in;
 (
-  nwr["amenity"="school"](area.in)(area.area);
+  nwr["amenity"="school"](area.in)(area.out);
 );
 out count;
 `;
@@ -95,6 +93,5 @@ const findWardWithMostSchools = async (): Promise<string> => {
   }
   return wardWithMostSchools;
 };
-
 
 export default findWardWithMostSchools;
